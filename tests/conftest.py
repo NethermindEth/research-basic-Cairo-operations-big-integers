b83bbbbd9a2af5fe68e94b60777f63d4538ff545
albert-garreta <mtdenm@gmail.com>
2022-05-31 23:59:18 +0200

    added is_square function

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,7 @@ from starkware.starknet.compiler.compile import compile_starknet_files
 
 # Some test fixtures are defined here
 
+UINT384_CONTRACT = os.path.join("contracts", "uint384_contract.cairo")
 UINT384_EXTENSION_CONTRACT = os.path.join("contracts", "uint384_extension_contract.cairo")
 FIELD_ARITHMETIC_CONTRACT = os.path.join("contracts", "field_arithmetic_contract.cairo")
 
@@ -20,6 +21,16 @@ async def starknet_factory():
     starknet = await Starknet.empty()
     return starknet
 
+
+@pytest.fixture(scope="module")
+async def uint384_contract(starknet_factory):
+    starknet = starknet_factory
+    # Deploy the account contract
+    contract_def = compile_starknet_files(
+        files=[UINT384_CONTRACT], disable_hint_validation=True
+    )
+    contract = await starknet.deploy(contract_def=contract_def)
+    return contract
 
 @pytest.fixture(scope="module")
 async def uint384_extension_contract(starknet_factory):
