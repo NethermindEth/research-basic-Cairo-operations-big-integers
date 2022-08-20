11a7d68a4e0777627eaef3b6843f17a67a50e675
albert-garreta <mtdenm@gmail.com>
2022-08-20 08:54:04 +0200

    warning on new verrsion of asyncio breaking the repo

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,9 @@ from starkware.starknet.compiler.compile import compile_starknet_files
 # Some test fixtures are defined here
 
 UINT384_CONTRACT = os.path.join("contracts", "uint384_contract.cairo")
-UINT384_EXTENSION_CONTRACT = os.path.join("contracts", "uint384_extension_contract.cairo")
+UINT384_EXTENSION_CONTRACT = os.path.join(
+    "contracts", "uint384_extension_contract.cairo"
+)
 FIELD_ARITHMETIC_CONTRACT = os.path.join("contracts", "field_arithmetic_contract.cairo")
 
 
@@ -26,20 +28,21 @@ async def starknet_factory():
 async def uint384_contract(starknet_factory):
     starknet = starknet_factory
     # Deploy the account contract
-    contract_class = compile_starknet_files(
-        files=[UINT384_CONTRACT], disable_hint_validation=True
-    )
-    contract = await starknet.deploy(contract_class=contract_class)
+    # contract_class = compile_starknet_files(
+    #     files=[UINT384_CONTRACT], disable_hint_validation=True
+    # )
+    contract = await starknet.deploy(source=UINT384_CONTRACT,)
     return contract
+
 
 @pytest.fixture(scope="module")
 async def uint384_extension_contract(starknet_factory):
     starknet = starknet_factory
     # Deploy the account contract
-    contract_class = compile_starknet_files(
-        files=[UINT384_EXTENSION_CONTRACT], disable_hint_validation=True
-    )
-    contract = await starknet.deploy(contract_class=contract_class)
+    # contract_class = compile_starknet_files(
+    #     files=[UINT384_EXTENSION_CONTRACT], disable_hint_validation=True
+    # )
+    contract = await starknet.deploy(source=UINT384_EXTENSION_CONTRACT,)
     return contract
 
 
@@ -47,8 +50,8 @@ async def uint384_extension_contract(starknet_factory):
 async def field_arithmetic_contract(starknet_factory):
     starknet = starknet_factory
     # Deploy the account contract
-    contract_class = compile_starknet_files(
-        files=[FIELD_ARITHMETIC_CONTRACT], disable_hint_validation=True
-    )
-    contract = await starknet.deploy(contract_class=contract_class)
+    # contract_class = compile_starknet_files(
+    #     files=[FIELD_ARITHMETIC_CONTRACT], disable_hint_validation=True
+    # )
+    contract = await starknet.deploy(source=FIELD_ARITHMETIC_CONTRACT,)
     return contract
